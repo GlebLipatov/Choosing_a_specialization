9047b0300ce83926b7eda32be38327eabc5361ce
Gleb Lipatov <it.lipatov@gmail.com>
2023-03-21 19:33:08 +0300

    All done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] words = {"hello", "2000", "world", ":0-)"};
+string strings = string.Empty;
+string[] result;
+
+foreach(string word in words)
+{
+    if (word.Length <= 3) strings += $"{word} ";
+}
+
+result = strings.Split(" ");
